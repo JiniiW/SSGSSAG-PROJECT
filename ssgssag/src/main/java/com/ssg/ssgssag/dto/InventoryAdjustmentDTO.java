51e51fece1e008e7d4c46ef68082968b18a14ff1
JiniiW <ksjine2@gmail.com>
2024-06-18 16:55:59 +0900

    style : 불필요한 주석 제거

--- ssgssag/src/main/java/com/ssg/ssgssag/dto/InventoryAdjustmentDTO.java
+++ ssgssag/src/main/java/com/ssg/ssgssag/dto/InventoryAdjustmentDTO.java
@@ -8,10 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 public class InventoryAdjustmentDTO {
     private Long pkInventorySeq; //해당하는 재고 번호
-
-    @Getter
     private String vInventoryChangeType; //변경 구분 타입 입고 or 출고
-    @Getter
     private int nInventoryShippingCnt; //변동 수량
 
     public String getvInventoryChangeType() {
--- ssgssag/src/main/java/com/ssg/ssgssag/dto/InventoryMovementDTO.java
+++ ssgssag/src/main/java/com/ssg/ssgssag/dto/InventoryMovementDTO.java
@@ -8,10 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 public class InventoryMovementDTO {
     private Long pkInventorySeq; //해당하는 재고 번호
-
-    @Getter
     private String vWarehouseNm2;
-    @Getter
     private String vZoneNm2;
 
     public String getvWarehouseNm2() {
